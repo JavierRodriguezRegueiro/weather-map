1e1411c82c512d027eddb3074a6a54fe212058ce
javierrodriguezregueiro <javierrodriguezregueiro@gmail.com>
2020-02-03 23:39:01 +0100

    Added new test to App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
+
+configure({adapter: new Adapter()});
+
+describe('<App \>', () => {
+    const mockState = {
+        lat: 42.81687,
+        lng: -1.64323,
+        zoom: 10
+    };
+    let app;
+    beforeEach(function () {
+        app = shallow(<App/>)
+    });
+    it('Render component without crash', () => {
+        expect(app.state('lat')).toEqual(mockState.lat);
+        expect(app.state('lng')).toEqual(mockState.lng);
+        expect(app.state('zoom')).toEqual(mockState.zoom);
+    });
+
+    it('Change state using setCenter', () => {
+        const componentInstance = app.instance();
+        componentInstance.setCenter(mockState.lng + 3, mockState.lat + 3);
+        expect(app.state('lat')).not.toBe(mockState.lat);
+        expect(app.state('lng')).not.toBe(mockState.lng);
+        expect(app.state('zoom')).toEqual(mockState.zoom);
+
+        expect(app.state('lat')).toEqual(mockState.lat + 3);
+        expect(app.state('lng')).toEqual(mockState.lng + 3);
+    })
 });
