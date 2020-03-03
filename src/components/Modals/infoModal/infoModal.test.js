0bcb488ee7b8c8077fedd14957614e0441a7cb2a
Javier Rodríguez Regueiro <javierrodriguezregueiro@gmail.com>
2020-03-03 15:02:38 +0100

    Remove unused deprencency

--- src/components/Modals/infoModal/infoModal.test.js
+++ src/components/Modals/infoModal/infoModal.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {InfoModal} from './infoModal';
-import {CityInput} from "../../cityInput/cityInput";
 
 configure({adapter: new Adapter()});
 
